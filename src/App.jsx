11ffc07e0e4b29901edcb7cb0531424cadbb1124
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-03-23 03:40:09 +0800

    scaffold capstone app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt=""> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt=""> */}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt=""> */}
+          <div className="category-body-container">
+            <h2>Sneaker</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt=""> */}
+          <div className="category-body-container">
+            <h2>Womens</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" alt=""> */}
+          <div className="category-body-container">
+            <h2>Men</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
